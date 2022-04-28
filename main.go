75474e176cdd62df0213c1ad645c6c793889da40
Distortions81 <carlotto81@gmail.com>
2022-04-28 13:58:35 -0600

    changed to color

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math"
 
+	"github.com/PerformLine/go-stockutil/colorutil"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
@@ -21,11 +22,13 @@ const (
 	escapeVal   = 4.0
 	camZoomDiv  = 1
 	wheelMult   = 4
-	gamma       = 0.454545
+
+	colorSat         = 0.8
+	colorBright      = 0.75
+	colorDegPerInter = 10
 )
 
 var (
-	palette      [maxIters + 1]uint8
 	renderWidth  int = winWidth / pixMag
 	renderHeight int = winHeight / pixMag
 
@@ -35,10 +38,10 @@ var (
 	zoomInt                int     = startOffset
 	lastMouseX, lastMouseY int
 
-	camX, camY float64
-
-	minBright uint8 = 0xff
-	maxBright uint8 = 0x00
+	camX, camY   float64
+	fsy, sStep   float64
+	tX, tY       int
+	diffX, diffY int
 )
 
 type Game struct {
@@ -46,10 +49,10 @@ type Game struct {
 
 func (g *Game) Update() error {
 
-	tX, tY := ebiten.CursorPosition()
+	tX, tY = ebiten.CursorPosition()
 	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
-		diffX := (tX - lastMouseX)
-		diffY := (tY - lastMouseY)
+		diffX = (tX - lastMouseX)
+		diffY = (tY - lastMouseY)
 
 		camX += ((float64(diffX) / camZoomDiv) / (float64(zoomInt) * curZoom))
 		camY += ((float64(diffY) / camZoomDiv) / (float64(zoomInt) * curZoom))
@@ -58,14 +61,14 @@ func (g *Game) Update() error {
 	lastMouseX = tX
 	lastMouseY = tY
 
-	_, fsy := ebiten.Wheel()
+	_, fsy = ebiten.Wheel()
 	if fsy > 0 {
 		zoomInt += wheelMult
 	} else if fsy < 0 {
 		zoomInt -= wheelMult
 	}
 
-	sStep := float64(zoomInt) / zoomDiv
+	sStep = float64(zoomInt) / zoomDiv
 	curZoom = (math.Pow(sStep, zoomPow))
 	return nil
 }
@@ -91,67 +94,36 @@ func main() {
 
 	offscreen = ebiten.NewImage(renderWidth, renderHeight)
 
-	for i := range palette {
-		palette[i] = uint8(math.Pow(float64(i)/float64(maxIters), gamma) * float64(0xff))
-	}
-
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
 }
 
+var r, g, b uint8
+var x, y float64
+var c, z complex128
+var j, i, it int
+
 func updateOffscreen() {
-	minBright = 0xff
-	maxBright = 0x00
 
-	for j := 0; j < renderWidth; j++ {
+	for j = 0; j < renderWidth; j++ {
 
-		for i := 0; i < renderHeight; i++ {
-			x := ((float64(j)/float64(renderWidth) - 0.5) / curZoom) - camX
-			y := ((float64(i)/float64(renderWidth) - 0.5) / curZoom) - camY
-			c := complex(x, y) //Rotate
-			z := complex(0, 0)
-			var it uint8
+		for i = 0; i < renderHeight; i++ {
+			x = ((float64(j)/float64(renderWidth) - 0.5) / curZoom) - camX
+			y = ((float64(i)/float64(renderWidth) - 0.5) / curZoom) - camY
+			c = complex(x, y) //Rotate
+			z = complex(0, 0)
+
 			for it = 0; it < maxIters; it++ {
 				z = z*z + c
 				if real(z)*real(z)+imag(z)*imag(z) > escapeVal {
 					break
 				}
 			}
-			if palette[it] < minBright {
-				minBright = palette[it]
-			}
-			if palette[it] > maxBright {
-				maxBright = palette[it]
-			}
-			offscreen.Set(j, i, color.RGBA{palette[it], palette[it], palette[it], 0xff})
+			r, g, b = colorutil.HsvToRgb(math.Mod(float64(it*colorDegPerInter), 360), colorSat, colorBright)
+			offscreen.Set(j, i, color.RGBA{r, g, b, 0xff})
 		}
 
-	}
-
-	if minBright > 200 {
-		minBright = 200
-	}
-	if maxBright < 201 {
-		maxBright = 201
-	}
-
-	/*Auto contrast*/
-	if minBright < 255 && minBright > 0 {
-		for j := 0; j < renderWidth; j++ {
-			for i := 0; i < renderHeight; i++ {
-				pixel := offscreen.At(j, i)
-				r, _, _, _ := pixel.RGBA()
-				dim := uint8(r>>8) - minBright //Subtract so black is black
-				if dim > 0 {
-					//Increase constast
-					out := uint8(float64(dim) / (float64(255-minBright-(255-maxBright)) / 255.0))
-					offscreen.Set(j, i, color.RGBA{out, out, out, 0xff})
-				} else {
-					offscreen.Set(j, i, color.RGBA{0, 0, 0, 0xff})
-				}
-			}
-		}
 	}
 }
 
