d0fa2c55bf7237226c0e09887987455956757926
Distortions81 <carlotto81@gmail.com>
2022-04-28 17:39:02 -0600

    fix output issues

--- main.go
+++ main.go
@@ -19,17 +19,17 @@ import (
 const (
 	chromaMode       = true
 	lumaMode         = true
-	autoZoom         = false
-	startOffset      = 985
-	superSample      = 1
-	windowDivisor    = 1.0
-	winWidth         = 512
-	winHeight        = 512
+	autoZoom         = true
+	startOffset      = 9800
+	superSample      = 8
+	windowDivisor    = 4.0
+	winWidth         = 3840
+	winHeight        = 2160
 	maxIters         = 10000
 	offX             = 0.747926709975882
 	offY             = -0.10785035275635992
 	zoomPow          = 100
-	zoomDiv          = 1000.0
+	zoomDiv          = 10000.0
 	escapeVal        = 4.0
 	colorDegPerInter = 10
 
@@ -66,35 +66,38 @@ type Game struct {
 
 func (g *Game) Update() error {
 
-	tX, tY := ebiten.CursorPosition()
-	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
-		diffX := (tX - lastMouseX)
-		diffY := (tY - lastMouseY)
+	if !autoZoom {
+		tX, tY := ebiten.CursorPosition()
+		if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+			diffX := (tX - lastMouseX)
+			diffY := (tY - lastMouseY)
 
-		camX += ((float64(diffX) / camZoomDiv) / (float64(zoomInt) * curZoom))
-		camY += ((float64(diffY) / camZoomDiv) / (float64(zoomInt) * curZoom))
+			camX += ((float64(diffX) / camZoomDiv) / (float64(zoomInt) * curZoom))
+			camY += ((float64(diffY) / camZoomDiv) / (float64(zoomInt) * curZoom))
+		}
+
+		lastMouseX = tX
+		lastMouseY = tY
+
+		_, fsy := ebiten.Wheel()
+		if fsy > 0 {
+			zoomInt += wheelMult
+		} else if fsy < 0 {
+			zoomInt -= wheelMult
+		}
+
+		sStep := float64(zoomInt) / zoomDiv
+		curZoom = (math.Pow(sStep, zoomPow))
 	}
-
-	lastMouseX = tX
-	lastMouseY = tY
-
-	_, fsy := ebiten.Wheel()
-	if fsy > 0 {
-		zoomInt += wheelMult
-	} else if fsy < 0 {
-		zoomInt -= wheelMult
-	}
-
-	sStep := float64(zoomInt) / zoomDiv
-	curZoom = (math.Pow(sStep, zoomPow))
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 
-	op := &ebiten.DrawImageOptions{Filter: ebiten.FilterNearest}
+	op := &ebiten.DrawImageOptions{Filter: ebiten.FilterLinear}
+	op.GeoM.Reset()
 	op.GeoM.Translate(0, 0)
-	op.GeoM.Scale(1.0/windowDivisor, 1.0/windowDivisor)
+	op.GeoM.Scale(1/windowDivisor, 1/windowDivisor)
 	screen.DrawImage(downresChroma, op)
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %0.2f, UPS: %0.2f, x: %v, y: %v z: %v", ebiten.CurrentFPS(), ebiten.CurrentTPS(), camX, camY, zoomInt))
 }
@@ -108,6 +111,61 @@ func main() {
 	ebiten.SetWindowTitle("Mandelbrot (Ebiten Demo)")
 	ebiten.SetFPSMode(ebiten.FPSModeVsyncOn)
 
+	camX = offX
+	camY = offY
+
+	buf := fmt.Sprintf("Threads found: %v", numThreads)
+	fmt.Println(buf)
+	if numThreads < 1 {
+		numThreads = 1
+	}
+
+	fmt.Println("Allocating image...")
+
+	/* Max image size */
+	if renderWidth > 32768 {
+		renderWidth = 32768
+		fmt.Println("renderWidth > 32768, truncating...")
+	}
+	if renderHeight > 32768 {
+		renderHeight = 32768
+		fmt.Println("renderHeight > 32768, truncating...")
+	}
+
+	offscreen = image.NewRGBA(image.Rect(0, 0, renderWidth, renderHeight))
+	offscreenGray = image.NewGray16(image.Rect(0, 0, renderWidth, renderHeight))
+	downresChroma = ebiten.NewImage(renderWidth/superSample, renderHeight/superSample)
+	downresLuma = ebiten.NewImage(renderWidth/superSample, renderHeight/superSample)
+
+	if offscreen == nil || offscreenGray == nil || downresChroma == nil || downresLuma == nil {
+		fmt.Println("Failed to allocate image")
+		return
+	}
+	fmt.Printf("complete!\n")
+
+	fmt.Printf("Building gamma table...")
+	swg := sizedwaitgroup.New((numThreads))
+	for i := range palette {
+		swg.Add()
+		go func(i int) {
+			defer swg.Done()
+
+			palette[i] = uint16(math.Pow(float64(i)/float64(maxIters), gamma) * float64(0xffff))
+		}(i)
+	}
+
+	swg.Wait()
+	fmt.Printf("complete!\n")
+
+	sStep := (float64(zoomInt) / zoomDiv)
+	curZoom = (math.Pow(sStep, zoomPow))
+
+	go func() {
+		for {
+			updateOffscreen()
+		}
+	}()
+
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
@@ -118,16 +176,17 @@ func updateOffscreen() {
 	swg := sizedwaitgroup.New(numThreads)
 	maxBright = 0x0000
 	minBright = 0xffff
-
 	for j := 0; j < renderWidth; j++ {
 		swg.Add()
 		go func(j int) {
 			defer swg.Done()
 			for i := 0; i < renderHeight; i++ {
+
 				x := ((float64(j)/float64(renderWidth) - 0.5) / curZoom) - camX
-				y := ((float64(i)/float64(renderWidth) - 0.5) / curZoom) - camY
+				y := ((float64(i)/float64(renderWidth) - 0.25) / curZoom) - camY
 				c := complex(x, y) //Rotate
 				z := complex(0, 0)
+
 				var it uint16
 				for it = 0; it < maxIters; it++ {
 					z = z*z + c
@@ -135,10 +194,10 @@ func updateOffscreen() {
 						break
 					}
 				}
-				if chromaMode {
-					r, g, b := colorutil.HsvToRgb(math.Mod(float64(it*colorDegPerInter), 360), 1.0, 1.0)
-					offscreen.Set(j, i, color.RGBA{r, g, b, 255})
-				}
+
+				r, g, b := colorutil.HsvToRgb(math.Mod(float64(it*colorDegPerInter), 360), 1.0, 1.0)
+				offscreen.Set(j, i, color.RGBA{r, g, b, 255})
+
 				if lumaMode {
 					offscreenGray.Set(j, i, color.Gray16{palette[it]})
 					if it > maxBright {
@@ -179,11 +238,11 @@ func updateOffscreen() {
 		/*Auto contrast*/
 		if lumaMode {
 			//Auto constrast limits
-			if minBright > 200 {
-				minBright = 200
+			if minBright > 51000 {
+				minBright = 51000
 			}
-			if maxBright < 201 {
-				maxBright = 201
+			if maxBright < 51255 {
+				maxBright = 51255
 			}
 
 			for j := 0; j < renderWidth; j++ {
@@ -197,7 +256,7 @@ func updateOffscreen() {
 						dim := y - minBright //Subtract so black is black
 						if dim > 0 {
 							//Increase constast
-							out := uint16(float64(dim) / (float64(255-minBright-(255-maxBright)) / 255.0))
+							out := uint16(float64(dim) / (float64(65535-minBright-(65535-maxBright)) / 65535.0))
 							offscreenGray.Set(j, i, color.Gray16{out})
 						} else {
 							offscreenGray.Set(j, i, color.Gray16{0})
@@ -207,9 +266,8 @@ func updateOffscreen() {
 				}(j)
 			}
 
-		}
-		swg.Wait()
-		if lumaMode {
+			swg.Wait()
+
 			downresLuma.DrawImage(ebiten.NewImageFromImage(offscreenGray), op)
 
 			fileName := fmt.Sprintf("out/luma-%v.tif", zoomInt)
@@ -229,51 +287,5 @@ func updateOffscreen() {
 
 func init() {
 
-	camX = offX
-	camY = offY
-
-	buf := fmt.Sprintf("Threads found: %v", numThreads)
-	fmt.Println(buf)
-	if numThreads < 1 {
-		numThreads = 1
-	}
-
-	fmt.Println("Allocating image...")
-
-	/* Max image size */
-	if renderWidth > 32768 {
-		renderWidth = 32768
-		fmt.Println("renderWidth > 32768, truncating...")
-	}
-	if renderHeight > 32768 {
-		renderHeight = 32768
-		fmt.Println("renderHeight > 32768, truncating...")
-	}
-
-	offscreen = image.NewRGBA(image.Rect(0, 0, renderWidth, renderHeight))
-	offscreenGray = image.NewGray16(image.Rect(0, 0, renderWidth, renderHeight))
-	downresChroma = ebiten.NewImage(renderWidth/superSample, renderHeight/superSample)
-	downresLuma = ebiten.NewImage(renderWidth/superSample, renderHeight/superSample)
-
-	fmt.Printf("complete!\n")
-
-	fmt.Printf("Building gamma table...")
-	swg := sizedwaitgroup.New((numThreads))
-	for i := range palette {
-		swg.Add()
-		go func(i int) {
-			defer swg.Done()
-
-			palette[i] = uint16(math.Pow(float64(i)/float64(maxIters), gamma) * float64(0xffff))
-		}(i)
-	}
-
-	swg.Wait()
-	fmt.Printf("complete!\n")
-
-	go func() {
-		for {
-			updateOffscreen()
-		}
-	}()
+	//
 }
