25ef5d5a5d916fc3caf4fdd745b3f2557b7462aa
Distortions81 <carlotto81@gmail.com>
2022-05-06 16:48:30 -0600

    threaded

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"image/color"
 	"log"
 	"math"
+	"runtime"
+	"time"
 
-	"github.com/PerformLine/go-stockutil/colorutil"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/remeh/sizedwaitgroup"
 )
 
 const (
@@ -16,16 +18,12 @@ const (
 	winWidth    = 1024
 	winHeight   = 1024
 	pixMag      = 2
-	maxIters    = 255
+	maxIters    = 1000
 	zoomPow     = 100
 	zoomDiv     = 1000.0
 	escapeVal   = 4.0
 	camZoomDiv  = 1
-	wheelMult   = 4
-
-	colorSat         = 0.8
-	colorBright      = 0.75
-	colorDegPerInter = 10
+	wheelMult   = 6
 )
 
 var (
@@ -35,6 +33,7 @@ var (
 	offscreen *ebiten.Image
 
 	curZoom                float64 = 1.0
+	gamma                          = 0.8
 	zoomInt                int     = startOffset
 	lastMouseX, lastMouseY int
 
@@ -74,25 +73,40 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	updateOffscreen()
+	//updateOffscreen()
 
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Scale(pixMag, pixMag)
 	screen.DrawImage(offscreen, op)
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %0.2f (click drag to move, wheel to zoom)", ebiten.CurrentFPS()))
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %0.2f (click drag to move, wheel to zoom) %f,%f", ebiten.CurrentFPS(), camX, camY))
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return outsideWidth, outsideHeight
 }
 
+var wg sizedwaitgroup.SizedWaitGroup
+var palette [(maxIters) + 1]uint8
+
 func main() {
+	for i := range palette {
+		palette[i] = uint8(math.Pow(float64(i)/float64(maxIters), gamma) * float64(0xFF))
+	}
+	wg = sizedwaitgroup.New(runtime.NumCPU())
+
 	ebiten.SetWindowSize(winWidth, winHeight)
 	ebiten.SetWindowResizable(false)
 	ebiten.SetWindowTitle("Mandelbrot (Ebiten Demo)")
 	ebiten.SetFPSMode(ebiten.FPSModeVsyncOn)
 
 	offscreen = ebiten.NewImage(renderWidth, renderHeight)
+
+	go func() {
+		for {
+			updateOffscreen()
+			time.Sleep(time.Millisecond)
+		}
+	}()
 
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
@@ -106,27 +120,33 @@ var j, i, it int
 
 func updateOffscreen() {
 
+	//offscreen.Clear()
 	for j = 0; j < renderWidth; j++ {
 
-		for i = 0; i < renderHeight; i++ {
-			x = ((float64(j)/float64(renderWidth) - 0.5) / curZoom) - camX
-			y = ((float64(i)/float64(renderWidth) - 0.5) / curZoom) - camY
-			c = complex(x, y) //Rotate
-			z = complex(0, 0)
+		wg.Add()
+		go func(j int) {
+			defer wg.Done()
 
-			for it = 0; it < maxIters; it++ {
-				z = z*z + c
-				if real(z)*real(z)+imag(z)*imag(z) > escapeVal {
-					break
+			for i = 0; i < renderHeight; i++ {
+				x = ((float64(j)/float64(renderWidth) - 0.5) / curZoom) - camX
+				y = ((float64(i)/float64(renderWidth) - 0.5) / curZoom) - camY
+				c = complex(x, y) //Rotate
+				z = complex(0, 0)
+
+				for it = 0; it < maxIters; it++ {
+					z = z*z + c
+					if real(z)*real(z)+imag(z)*imag(z) > escapeVal {
+						offscreen.Set(j, i, color.RGBA{palette[it], palette[it], palette[it], 0xFF})
+						break
+					}
+					offscreen.Set(j, i, color.RGBA{0, 0, 0, 0xFF})
 				}
+				//offscreen.Set(j, i, color.RGBA{palette[it], palette[it], palette[it], 0xFF})
 			}
-			r, g, b = colorutil.HsvToRgb(math.Mod(float64(it*colorDegPerInter), 360), colorSat, colorBright)
-			offscreen.Set(j, i, color.RGBA{r, g, b, 0xff})
-		}
-
+		}(j)
+		wg.Wait()
 	}
 }
 
 func init() {
-	//
 }
