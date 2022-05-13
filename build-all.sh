535e3eba296106125207e32204c2e15605ce1f2b
Distortions81 <carlotto81@gmail.com>
2022-05-13 17:29:43 -0600

    update binaries

--- build-all.sh
+++ build-all.sh
@@ -9,3 +9,9 @@ zip -m golang-frac-win64.zip golang-frac-win64.exe
 
 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o golang-frac-linux64.bin
 zip -m golang-frac-linux64.zip golang-frac-linux64.bin
+
+rm wasm/main.wasm.gz
+rm wasm/main.wasm
+
+GOGC=10 GOOS=js GOARCH=wasm go build -ldflags="-s -w" -o wasm/main.wasm
+gzip -9 wasm/main.wasm
