fc659f24ff968857803467b0f7b26f85a4dbec01
Distortions81 <carlotto81@gmail.com>
2022-05-14 11:01:35 -0600

    render script

---
+++ render.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nice -n 20 ./golang-frac -colorBrightness 0.5 -colorDegPerInter 1 -colorSaturation 0.8 -doChroma true -doLuma true -end 3600 -escape 4 -gammaChroma 1 -gammaLuma 1 -height 3840 -iters 2500 -offx -0.2925598845093559 -offy -0.2925598845093559 -outDir out -sleep -sleepMicro 100 -super 8 -width 3840 -workBlock 128 -zSleepDiv 1.1 -zoom 100 -zoomAdd 1
