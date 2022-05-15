a43030b1992a8482194587afbbf22a9575ac158f
Distortions81 <carlotto81@gmail.com>
2022-05-14 20:02:11 -0600

    fix typo

--- render-bg.sh
+++ render-bg.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nice -n 20 ./golang-frac -colorBrightness 0.5 -colorDegPerInter 1 -colorSaturation 0.8 -end 3600 -escape 4 -gammaChroma 1 -gammaLuma 1 -height 3840 -iters 2500 -offx -0.2925598845093559 -offy -0.45788116850031885 -outDir out -doSleep -sleepMicro 1000 -super 8 -width 3840 -workBlock 128 -zSleepDiv 1.1 -zoom 100 -zoomAdd 1
+nice -n 20 ./golang-frac -colorBrightness 0.5 -colorDegPerInter 1 -colorSaturation 0.8 -end 3600 -escape 4 -gammaChroma 1 -gammaLuma 1 -height 3840 -iters 2500 -offx -0.2925598845093559 -offy -0.45788116850031885 -outDir out -doSleep -sleepMicro 1000 -super 8 -width 3840 -workBlock 128 -zSpeedDiv 1.1 -zoom 100 -zoomAdd 1
