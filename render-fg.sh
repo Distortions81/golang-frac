d5986a0461c9c8239cbbf0400cb3317bae1e9a55
carlotto81 <carlotto81@gmail.com>
2023-03-09 10:08:55 -0700

    limit frames end

--- render-fg.sh
+++ render-fg.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./golang-frac -start 500 -end 4500 -offx 1.4789066795775936 -offy 0.002544532441416673 -super 8
+./golang-frac -start 500 -end 3500 -offx 1.4789066795775936 -offy 0.002544532441416673 -super 8
